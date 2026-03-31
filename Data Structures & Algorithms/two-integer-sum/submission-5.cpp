73e5aeb303090a0818789bd27696c74cea1ce42b
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-03-31 21:59:40 +0100

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> my_map;
+        my_map.reserve(nums.size());
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (auto it = my_map.find(target - nums[i]); it != my_map.end()) {
+                return {it->second, i};
+            } else {
+                my_map[nums[i]] = i;
+            }
+        }
+        return {0,0};
+    }
+};
