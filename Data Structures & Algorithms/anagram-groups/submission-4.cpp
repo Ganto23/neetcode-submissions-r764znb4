2b565a19b68fef819ee8b820eff7123e1ce58453
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-04-01 14:37:25 +0100

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        std::unordered_map<std::string, std::vector<std::string>> my_map;
+        my_map.reserve(strs.size());
+
+        for (auto str : strs) {
+            std::string cpy = str;
+            std::sort(str.begin(), str.end());
+            if (my_map.contains(str)){
+                my_map[str].push_back(cpy);
+            } else {
+                my_map[str] = {cpy};
+            }
+        }
+
+        std::vector<std::vector<std::string>> output;
+        output.reserve(my_map.size());
+        for (auto it = my_map.begin(); it != my_map.end(); it++) {
+            output.push_back(it->second);
+        }
+        return output;
+    }
+};
