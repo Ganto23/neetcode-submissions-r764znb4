efa66ef824ca418574725183a01dc07eebd5fec6
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-04-02 14:43:44 +0100

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        stringstream ss;
+        for (const auto& str : strs) {
+            ss << str.size() << "#" << str;
+        }
+        return ss.str();
+    }
+
+    vector<string> decode(string s) {
+        vector<string> output;
+        stringstream ss(s);
+        string len_str;
+        
+        while (getline(ss, len_str, '#')){
+            int len = stoi(len_str);
+            string word(len, ' ');
+            ss.read(word.data(), len);
+            output.push_back(std::move(word));
+        }
+        return output;
+    }
+};
