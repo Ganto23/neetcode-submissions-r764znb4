e42b52139839fdb37913d018cea8017da93f1031
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-04-12 16:08:35 +0100

    Add: minimum-stack - submission-8

---
+++ Data Structures & Algorithms/minimum-stack/submission-8.cpp
@@ -0,0 +1,25 @@
+class MinStack {
+    std::vector<int> my_stack;
+    std::list<int> min_element;
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        if (min_element.empty() || val <= min_element.back()) min_element.push_back(val);
+        my_stack.push_back(val);
+    }
+    
+    void pop() {
+        if (my_stack.back() == min_element.back()) min_element.pop_back();
+        my_stack.pop_back();
+    }
+    
+    int top() {
+        return my_stack.back();
+    }
+    
+    int getMin() {
+        return min_element.back();
+    }
+};
