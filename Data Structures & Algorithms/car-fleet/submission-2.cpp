c4f77203f45af34ae7dbc1810029cbada0d855e6
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-04-14 13:53:18 +0100

    Add: car-fleet - submission-2

---
+++ Data Structures & Algorithms/car-fleet/submission-2.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        std::vector<double> time_stack;
+        time_stack.reserve(position.size());
+
+        std::vector<int> indices(position.size());
+        std::iota(indices.begin(), indices.end(), 0);
+        std::sort(indices.begin(), indices.end(), [&](int i, int j) {
+            return position[i] > position[j]; 
+        });
+
+        for (auto idx : indices) {
+            double cur_time = static_cast<double>(target - position[idx]) / speed[idx];
+            if (time_stack.empty()) {
+                time_stack.push_back(cur_time);
+            } else {
+                double prev_time = time_stack.back();
+                if (cur_time > prev_time) {
+                    time_stack.push_back(cur_time);
+                }
+            }
+        }
+        return time_stack.size();
+    }
+};
