f72a868af7848e9b713db43c79b9d8ca77579b77
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-04-16 14:17:12 +0100

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool verify(const auto& piles, int er, int h) {
+        long long cur_time = 0;
+        for (const auto& bananas : piles) {
+            cur_time += (bananas + er - 1) / er;
+        }
+        return (cur_time > h) ? false : true;
+    }
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1;
+        int r = *std::max_element(piles.begin(), piles.end());
+        int best_er = 0;
+
+        while (l <= r) {
+            int mid = l + (r-l)/2;
+            if (verify(piles, mid, h)) {
+                r = mid - 1;
+                best_er = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return best_er;
+    }
+};
