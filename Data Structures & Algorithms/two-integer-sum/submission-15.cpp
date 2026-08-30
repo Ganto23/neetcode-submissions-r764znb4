dfdcb17c3a2bdf0d4776bac6225d751f9f196316
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-08-30 15:37:39 +0100

    Update: two-integer-sum - submission-15

--- Data Structures & Algorithms/two-integer-sum/submission-15.cpp
+++ Data Structures & Algorithms/two-integer-sum/submission-15.cpp
@@ -1,28 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        std::vector<std::pair<int, int>> indexed_nums;
-        indexed_nums.reserve(nums.size());
-        for(int i = 0; i < nums.size(); i++){
-            indexed_nums.emplace_back(nums[i], i);
-        }
-        std::sort(indexed_nums.begin(), indexed_nums.end());
-
-        int l = 0; int r = indexed_nums.size()-1;
-
-        while (l < r) {
-            const auto& in_l = indexed_nums[l]; const auto& in_r = indexed_nums[r];
-            int current_sum = in_l.first + in_r.first;
-            if (current_sum == target) {
-                if (in_l.second > in_r.second) {
-                    return {in_r.second, in_l.second};
-                } else {
-                    return {in_l.second, in_r.second};
-                } 
-            } else if (current_sum > target) {
-                r--;
+        std::unordered_map<int,int> mymap;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (mymap.contains(target - nums[i])) {
+                return {mymap[target-nums[i]], i};
             } else {
-                l++;
+                mymap[nums[i]] = i;
             }
         }
         return {};
