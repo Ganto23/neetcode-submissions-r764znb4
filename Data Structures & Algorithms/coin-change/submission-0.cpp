7178c872f2e16cc24a5a85160ce31d3fd8e82256
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-09-18 20:38:47 +0100

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<long long> dp(amount+1, INT_MAX);
+        dp[0] = 0;
+
+        for (long long i = 0; i < dp.size(); ++i) {
+            for (auto coin : coins) {
+                if (i - coin < 0) continue;
+                dp[i] = min(dp[i], dp[i-coin] + 1);
+            }
+        }
+        return (dp[amount] == INT_MAX) ? -1 : dp[amount];
+    }
+};
