34a65824cdce6cdf67d707e09d228b972eec920d
Godfrey Antomarlin <121034077+Ganto23@users.noreply.github.com>
2026-09-22 20:12:47 +0100

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_idx = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (max_idx < i) return false;
+            max_idx = max(max_idx, i + nums[i]);
+        }
+        return true;
+    }
+};
